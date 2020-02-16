9cf50e3b04d081d9edc2fc8e0f3c7d9e087cfd67
mateuszAiRKut <mateusz.w.air@gmail.com>
2020-02-16 20:25:43 +0100

    Add files via upload

---
+++ WpfAlarmClock/WpfAlarmClock/ClassClock.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WpfAlarmClock
+{
+    delegate void MyDlegateTimer();
+    class ClassClock
+    {
+        public ref byte Hour { get { return ref hour; } }
+        public ref byte Minute { get { return ref minute; } }
+        public ref byte Second { get { return ref second; } }
+
+        public ClassClock()
+        {
+           
+        }
+
+        public void AddActionClock(MyDlegateTimer action) //Action
+        {
+            delegateClock += action;
+        }
+
+        public void StartClock()
+        {
+            if (delegateClock == null)
+                return;
+            if(!isActive)
+            {
+                isActive = true;
+                /*timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Tick += timer_Tick;
+                timer.Start();*/
+                threadClock = new Thread(() => { delegateClock(); });
+                threadClock.Priority = ThreadPriority.Highest;
+                threadClock.Start();
+            }
+
+        }
+
+        public void StopClock()
+        {
+            isActive = false;
+            threadClock.Abort();
+            delegateClock = null;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        /*void timer_Tick(object sender, EventArgs e)
+        {
+            hour = (byte)DateTime.Now.Hour;
+            minute = (byte)DateTime.Now.Minute;
+            second = (byte)DateTime.Now.Second;
+            actualT.Clear();
+            actualT.Append(hour).Append(":").Append(minute).Append(":").Append(second);
+            actualTime.Text = actualT.ToString();
+        }*/
+
+        private MyDlegateTimer delegateClock;
+        private Thread threadClock;
+        private bool isActive;
+        private byte hour, minute, second;
+        //private Action objectD; ThreadStart objectD;
+    }
+}
---
+++ WpfAlarmClock/WpfAlarmClock/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+using Microsoft.Win32;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace WpfAlarmClock
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            byte i = 0;
+            for (; i < 24; i++)
+            {
+                setHours.Items.Add(i.ToString());              //new ComboBoxItem() { FontSize = 12, Content = i.ToString() }
+                setMinutes.Items.Add(i.ToString());
+            }
+            for (; i < 60; i++)
+                setMinutes.Items.Add(i.ToString());
+            actualT = new StringBuilder();
+            mediaPlayer = new MediaPlayer();
+            objVol = new CoreAudioController().DefaultPlaybackDevice;
+            objVol.Volume = 44;
+            objectClassClock = new ClassClock();
+            objectClassClock.AddActionClock(workTimer);
+            objectClassClock.StartClock();
+
+            //RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            //reg.SetValue("Application Alarm Clock", @"C:\Users\Mateusz\source\repos\WpfAlarmClock\WpfAlarmClock\bin\Debug\WpfAlarmClock.EXE");
+        }
+
+        void workTimer()
+        {
+            while (true)
+            {
+                objectClassClock.Hour = (byte)DateTime.Now.Hour;
+                objectClassClock.Minute = (byte)DateTime.Now.Minute;
+                objectClassClock.Second = (byte)DateTime.Now.Second;
+                actualT.Clear();
+                actualT.Append(objectClassClock.Hour).Append(":").Append(objectClassClock.Minute).Append(":").Append(objectClassClock.Second);
+                Dispatcher.Invoke(() => 
+                { 
+                    actualTime.Text = actualT.ToString();
+                    if (actualAlarm.Text != "")
+                    {
+                        if (objectClassClock.Hour == byte.Parse(setHours.Text) && objectClassClock.Minute >= byte.Parse(setMinutes.Text) && !setAnothetTime)
+                            mediaPlayer.Play();
+                    }
+                });              
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (setHours.Text == "" || setMinutes.Text == "")
+                return;
+            actualAlarm.Text = setHours.Text + ":" + setMinutes.Text;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            setAnothetTime = false;
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+            setAnothetTime = true;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (objectClassClock != null)
+            {
+                objectClassClock.StopClock();
+            }
+        }
+
+        private bool setAnothetTime;
+        private StringBuilder actualT;
+        private MediaPlayer mediaPlayer;
+        private CoreAudioDevice objVol;
+        private ClassClock objectClassClock;
+    }
+}
